0a7f214f243b4ae376521072198d324b71eec277
Shoxjahon1302 <shoxjahonniyatov@gmail.com>
2024-01-06 13:38:31 +0500

    sada

--- config/db.js
+++ config/db.js
@@ -1,16 +1,12 @@
 const mongoose = require("mongoose");
-const config = require("config");
-const db = config.get("mongoURI");
-
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
+    await mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false,
       useUnifiedTopology: true,
     });
-
     console.log("Connected to MongoDB");
   } catch (err) {
     console.error(err.message);
